7b96b701fb35c61c3cfdccb4163445aa1d75b70d
Jannis Feldmann <jannisfeldmann@MacBook-Air.sekbubikon.lan>
2023-02-28 08:42:14 +0100

    Added script

--- index.php
+++ index.php
@@ -9,13 +9,30 @@
 		<div class="flexbox">
         <label class="toggle">
         <input type="checkbox">
-        <span class="slider">
-            Hotspot
-        </span>
-        </label></div>
+        <span class="slider"></span>
+        </label>
+  <span class="label-text">Toggle Hotspot</span></div>
 		<div class="flexbox">Flexbox 2</div>
 		<div class="flexbox">Flexbox 3</div>
 		<div class="flexbox">Flexbox 4</div>
 	</div>
+
+    <script>
+    const toggleSwitch = document.querySelector('.toggle input');
+    toggleSwitch.addEventListener('change', function() {
+      const action = this.checked ? 'on' : 'off';
+      fetch('togglehotspot.php', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: 'action=' + action
+      }).then(function(response) {
+        console.log('Toggle switch ' + action);
+      }).catch(function(error) {
+        console.error('Error toggling switch:', error);
+      });
+    });
+  </script>
 </body>
 </html>
