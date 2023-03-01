83fcf426a1f552ac32cf58b05be82dec0c5985fd
jannisfeldi <jannis@jannisfeldmann.dev>
2023-03-01 17:53:49 +0000

    added back shadowsA

---
+++ getNetworkUsage.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+if(isset($_GET["type"])) {
+    if($_GET["type"] == "download") {
+        echo exec("ifconfig wlan0 | grep 'RX packets' |grep -o -P '\([^)]+\)$' | tr -d '()'");
+    }
+    if($_GET["type"] == "upload") {
+        echo exec("ifconfig wlan0 | grep 'TX packets' |grep -o -P '\([^)]+\)$' | tr -d '()'");
+    }
+}
+
+
+
+
+?>
