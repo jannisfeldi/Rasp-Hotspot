f48dc45861c3d0a54c0208ba7021065fc22c921f
jannisfeldi <jannis@jannisfeldmann.dev>
2023-03-03 06:14:23 +0000

    removed useless comment

--- getTroughput.php
+++ getTroughput.php
@@ -5,5 +5,4 @@ $tx = exec("sar -n DEV 1 1 | grep 'Average:' | grep 'eth0' | awk '{print $6}'");
 $rx = round($rx/1024);
 $tx = round($tx/1024);
 echo "↓".$rx."MB/s | ↑".$tx."MB/s";
-//useless comment
 ?>
