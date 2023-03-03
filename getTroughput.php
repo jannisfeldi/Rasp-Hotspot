2fd405a6e10d9e0ab65d3998d85fe1fd81939203
jannisfeldi <jannis@jannisfeldmann.dev>
2023-03-03 12:09:15 +0000

    updated network troughput to 2 floating numbers

--- getTroughput.php
+++ getTroughput.php
@@ -2,7 +2,7 @@
 $rx = exec("sar -n DEV 1 1 | grep 'Average:' | grep 'eth0' | awk '{print $5}'");
 $tx = exec("sar -n DEV 1 1 | grep 'Average:' | grep 'eth0' | awk '{print $6}'");
 
-$rx = round($rx/1024);
-$tx = round($tx/1024);
+$rx = round($rx/1024,2);
+$tx = round($tx/1024,2);
 echo "↓".$rx."MB/s | ↑".$tx."MB/s";
 ?>
