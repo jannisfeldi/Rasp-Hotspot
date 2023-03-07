b28a9d22d2fef64bab973bece3e92528a5b4c297
jannisfeldi <jannis@jannisfeldmann.dev>
2023-03-07 07:28:26 +0000

    not showing errors now

--- getCpuTemp.php
+++ getCpuTemp.php
@@ -1,7 +1,4 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 echo exec("cat /sys/class/thermal/thermal_zone0/temp | awk '{print $1/1000}'"),"°C";
 
 
