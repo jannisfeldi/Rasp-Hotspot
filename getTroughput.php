09c4778bed62af24010581822112345f41801f4c
jannisfeldi <jannis@jannisfeldmann.dev>
2023-03-09 14:44:51 +0000

    changed interface because of new antenna

--- getTroughput.php
+++ getTroughput.php
@@ -1,6 +1,6 @@
 <?php
-$rx = exec("sar -n DEV 1 1 | grep 'Average:' | grep 'eth0' | awk '{print $5}'");
-$tx = exec("sar -n DEV 1 1 | grep 'Average:' | grep 'eth0' | awk '{print $6}'");
+$rx = exec("sar -n DEV 1 1 | grep 'Average:' | grep 'wlan0' | awk '{print $5}'");
+$tx = exec("sar -n DEV 1 1 | grep 'Average:' | grep 'wlan0' | awk '{print $6}'");
 
 $rx = round($rx/1024,2);
 $tx = round($tx/1024,2);
--- togglehotspot.php
+++ togglehotspot.php
@@ -4,11 +4,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 if(isset($_GET['action'])) {
     if($_GET['action'] == "on") {
-        exec('sleep 5 && echo "on" > states/hotspot.state');
+        exec('sleep 5 && echo "on" > states/hotspot.state && sudo systemctl start wlanctl');
 
     }
     if($_GET['action'] == "off") {
-        exec('sleep 3 && echo "off" > states/hotspot.state');
+        exec('sleep 3 && echo "off" > states/hotspot.state && sudo systemctl stop wlanctl');
     }
 }
 ?>
