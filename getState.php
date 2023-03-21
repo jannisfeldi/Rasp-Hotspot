7bb8a672255bf744ba6f2f49766587f58cb21f4c
jannisfeldi <jannis@jannisfeldmann.dev>
2023-03-21 07:51:03 +0000

    Made some spelling changes

--- getState.php
+++ getState.php
@@ -1,3 +1,4 @@
 <?php
+
 echo exec("cat states/hotspot.state");
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>PiAccess Interface</title>
+	<title>PiAccess WebInt</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
